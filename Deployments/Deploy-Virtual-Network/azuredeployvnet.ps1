6e360c71d1ff0d7cad525354e9593390c0be58ec
github@mcitpnetworks.com <github@mcitpnetworks.com>
2022-01-13 18:42:55 -0500

    Upload Files

--- Deployments/Deploy-Virtual-Network/azuredeployvnet.ps1
+++ Deployments/Deploy-Virtual-Network/azuredeployvnet.ps1
@@ -7,3 +7,14 @@ New-AzResourceGroupDeployment -Name 'Deploy-VNet' -ResourceGroupName $ResourceGr
 
 # Direct Deployment
 New-AzResourceGroupDeployment -Name 'Deploy-VNet' -ResourceGroupName $ResourceGroupName -TemplateUri 'https://raw.githubusercontent.com/elliottfieldsjr/KillerHomeLab-Training/main/Deployments/Deploy-Virtual-Network/linkedtemplates/vnet.json'
+
+# Using Parameter Object
+$parameters = @{
+    vnetName = 'KHL-VNet3'
+    vnetPrefix = '10.3.0.0/16'
+    subnet1Name = 'KHL-VNet3-Subnet1'
+    subnet1Prefix = '10.3.1.0/24'
+    location = 'usgovtexas'
+}
+ 
+New-AzResourceGroupDeployment -Name 'Deploy-VNet' -ResourceGroupName $ResourceGroupName -TemplateUri 'https://raw.githubusercontent.com/elliottfieldsjr/KillerHomeLab-Training/main/Deployments/Deploy-Virtual-Network/linkedtemplates/vnet.json' -TemplateParameterObject $parameters
